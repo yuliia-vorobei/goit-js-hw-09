b5aef57d405545c11f46527c7204f602a67b5074
Yuliia Vorobei <vorobeju1@gmail.com>
2024-06-23 18:39:17 +0200

    fixed homework 9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,42 +1,52 @@
 formData = { email: '', message: '' };
 const feedbackForm = document.querySelector('.feedback-form');
-
 const storageData = localStorage.getItem('feedback-form-state');
+
+// Check if we have saved data
 if (storageData) {
   const userData = JSON.parse(storageData);
-  console.log(userData);
-  // console.log(feedbackForm.elements.email.value);
+
+  // Fill in the form with saved data
   feedbackForm.elements.email.value = userData.email;
   feedbackForm.elements.message.value = userData.message;
+
+  // Fill in the formData object with saved data
+  formData.email = userData.email;
+  formData.message = userData.message;
 }
 
 feedbackForm.addEventListener('input', handlerInput);
 function handlerInput(event) {
   event.preventDefault();
-  if (event.target.name) {
-    let inputValues = event.target.value;
-    console.log(inputValues);
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-  }
+
+  // Get the changed value and element name
+  let inputValues = event.target.value;
+  let inputName = event.target.name;
+
+  // Place value in formData object
+  formData[inputName] = inputValues;
+
+  // Place formData object in localstorage
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
 feedbackForm.addEventListener('submit', handlerSubmit);
 function handlerSubmit(event) {
   event.preventDefault();
+
+  // Retrieve values from form
   const form = event.target;
   const email = form.elements.email.value.trim();
   const message = form.elements.message.value.trim();
-  console.log(email);
-  console.log(message);
 
-  if (email === '' || message === '') {
-    alert('Fill please all fields');
+  // Check if the form is not empty
+  if (email != '' && message != '') {
+    // Log the formData object
+    console.log(formData);
+
+    // Clear localstorage, form, and formData object
+    localStorage.removeItem('feedback-form-state');
+    formData = { email: '', message: '' };
+    form.reset();
   }
-
-  formData.email = email;
-  formData.message = message;
-  console.log(formData);
-  localStorage.removeItem('feedback-form-state');
-  formData = { email: '', message: '' };
-  form.reset();
 }
